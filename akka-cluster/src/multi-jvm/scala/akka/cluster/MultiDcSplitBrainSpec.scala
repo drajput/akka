fb18792734c3813d7b3fdaff71043afa0da103d5
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-08-30 07:47:19 +0200

    fourth might generate UnreachableDataCenter in unsplit

--- akka-cluster/src/multi-jvm/scala/akka/cluster/MultiDcSplitBrainSpec.scala
+++ akka-cluster/src/multi-jvm/scala/akka/cluster/MultiDcSplitBrainSpec.scala
@@ -20,7 +20,8 @@ object MultiDcSplitBrainMultiJvmSpec extends MultiNodeConfig {
 
   commonConfig(ConfigFactory.parseString(
     """
-      akka.loglevel = INFO
+      akka.loglevel = DEBUG
+      akka.cluster.debug.verbose-heartbeat-logging = on
       akka.remote.netty.tcp.connection-timeout = 5 s # speedup in case of connection issue
       akka.remote.retry-gate-closed-for = 1 s
       akka.cluster.multi-data-center {
@@ -101,7 +102,7 @@ abstract class MultiDcSplitBrainSpec
     val memberNodes = (dc1 ++ dc2).filterNot(notMembers)
     val probe = TestProbe()
     runOn(memberNodes: _*) {
-      cluster.subscribe(probe.ref, classOf[DataCenterReachabilityEvent])
+      cluster.subscribe(probe.ref, classOf[ReachableDataCenter])
       probe.expectMsgType[CurrentClusterState]
     }
     enterBarrier(s"unsplit-$barrierCounter")
